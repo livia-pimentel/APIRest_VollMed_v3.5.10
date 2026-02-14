45a9787c96a410901dbd435bf21a61664a4d909e
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-14 17:12:35 -0300

    Add dependencia do spring doc e liberação das urls do spring doc no securityFilterChain

--- src/main/java/med/voll/api/Vollmed/Api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/Vollmed/Api/infra/security/SecurityConfigurations.java
@@ -30,6 +30,7 @@ public class SecurityConfigurations {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Desabilita o padrão statefull do login e senha que aparece na pagina
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers("/login").permitAll(); // Permite o acesso a página de login, as demais ficam bloqueadas até se autenticar
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
                     req.anyRequest().authenticated();// Qualquer outra rota, exceto login, precisa de autenticação
                 })
                 // Determina que o filtro da aplicação deve ser executada primeiro
